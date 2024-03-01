875a472736ffe1023f0be6d038844abbd17e3f2a
Renan Meneses de Andrade Franca <renan.andradefranca@gmail.com>
2024-03-01 10:47:31 -0300

    Apply springdoc-jwt module

---
+++ src/main/java/tech/jhipster/gradleapp/wire/springdoc/infrastructure/primary/SpringdocJWTConfiguration.java
@@ -0,0 +1,32 @@
+package tech.jhipster.gradleapp.wire.springdoc.infrastructure.primary;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import tech.jhipster.gradleapp.shared.generation.domain.ExcludeFromGeneratedCodeCoverage;
+
+@Configuration
+@AutoConfigureBefore(SpringdocConfiguration.class)
+@ExcludeFromGeneratedCodeCoverage(reason = "Not called by integration tests")
+class SpringdocJWTConfiguration {
+
+  private static final String SECURITY_SCHEME_NAME = "bearer-jwt";
+
+  @Bean
+  GlobalOpenApiCustomizer jwtOpenApi() {
+    return openApi ->
+      openApi.addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME)).components(jwtComponents(openApi.getComponents()));
+  }
+
+  private Components jwtComponents(Components existingComponents) {
+    return existingComponents.addSecuritySchemes(
+      SECURITY_SCHEME_NAME,
+      new SecurityScheme().name(SECURITY_SCHEME_NAME).type(SecurityScheme.Type.HTTP).scheme("Bearer").bearerFormat("JWT")
+    );
+  }
+}
